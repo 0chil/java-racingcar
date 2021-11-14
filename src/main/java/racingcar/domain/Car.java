7f7828c05fc7c5fccd58c280d8b5bc4465541c90
땡칠 <0@chll.it>
2021-11-14 15:10:57 +0900

    feat: 자동차 이름 부여 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 public class Car {
     private int location;
+    private String name;
 
     public Car(){
         this.location = 0;
@@ -11,11 +12,19 @@ public class Car {
         this.setLocation(this.getLocation() + 1);
     }
 
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
     public int getLocation() {
         return location;
     }
 
-    public void setLocation(int location) {
-        this.location = location;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -18,4 +18,16 @@ public class CarTest {
         // then
         Assertions.assertThat(car.getLocation()).isEqualTo(3);
     }
+
+    @Test
+    void 자동차_이름_붙이기(){
+        // given
+        Car car = new Car();
+
+        // when
+        car.setName("자동차 이름 붙이기!");
+
+        // then
+        Assertions.assertThat(car.getName()).isEqualTo("자동차 이름 붙이기!");
+    }
 }
