17605cd169b25aaed64ef1985e18206ef4c49a57
땡칠 <0@chll.it>
2021-11-14 16:45:07 +0900

    refactor: rename Car to Cars
    CarsController controls CarList, not just a Car

--- src/test/java/racingcar/controller/CarsControllerTest.java
+++ src/test/java/racingcar/controller/CarsControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CarControllerTest {
+public class CarsControllerTest {
 
     @Test
     void 자동차_N대_생성(){
