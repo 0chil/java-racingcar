12fa9772a7fc88783f9cfb9d22022258c3032558
땡칠 <0@chll.it>
2021-11-14 16:41:56 +0900

    refactor: make error message string constant

--- src/main/java/racingcar/utils/ValidationUtils.java
+++ src/main/java/racingcar/utils/ValidationUtils.java
@@ -4,10 +4,12 @@ import racingcar.exception.CarNameException;
 
 public class ValidationUtils {
 
-    public static void validateCarNameLength(String s) {
-        if (1 <= s.length() && s.length() <= 5) {
+    public static final String CAR_LENGTH_ERROR_MSG = "자동차 이름은 1자 이상 5자 이하여야 합니다.";
+
+    public static void validateCarNameLength(String carName) {
+        if (1 <= carName.length() && carName.length() <= 5) {
             return;
         }
-        throw new CarNameException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+        throw new CarNameException(CAR_LENGTH_ERROR_MSG);
     }
 }
