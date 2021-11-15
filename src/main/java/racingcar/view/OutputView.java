1c0d57bf0cc094a57fd96820c271575d65d5246f
땡칠 <0@chll.it>
2021-11-15 20:34:18 +0900

    feat: 게임 입력 요구 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -19,4 +19,12 @@ public class OutputView {
     public static void printGameResultString(String gameResultString) {
         System.out.println("실행 결과\n" + gameResultString);
     }
+
+    public static void printCarNameRequest(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public static void printIterateNumberRequest(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
