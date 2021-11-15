f597e50f2d6b6c516765deb0dc62c028d651b1f9
땡칠 <0@chll.it>
2021-11-15 20:41:50 +0900

    feat: 게임 시작, 결과 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,45 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.domain.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String [] args){
+    private static GameController gameController;
+
+    public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        gameController = new GameController();
+        OutputView.printCarNameRequest();
+        inputCars(scanner);
+        OutputView.printIterateNumberRequest();
+        inputIterateNumber(scanner);
+
+        gameController.runGame();
+
+        printGameResult();
+        printWinner();
+    }
+
+    public static void inputCars(final Scanner scanner){
+        List<String> carNameList = InputView.getUserCarNameInput(scanner);
+        gameController.addCars(carNameList);
+    }
+
+    public static void inputIterateNumber(final Scanner scanner){
+        int iterateNumber = InputView.getUserIterateNumberInput(scanner);
+        gameController.setIterateNumber(iterateNumber);
+    }
+
+    public static void printGameResult(){
+        OutputView.printGameResultString(gameController.toString());
+    }
+
+    public static void printWinner(){
+        OutputView.printWinner(gameController.getWinnerCarList());
     }
 }
